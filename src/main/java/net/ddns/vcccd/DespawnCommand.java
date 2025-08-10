cc7d5bb0aae339ba2b04cdfbe72577563ba34c5a
ApoloG6 <williamdrosado@yahoo.com>
2025-08-10 12:00:50 -0400

    Despawn Command QOL
    Added how far /despawnentities to be configurable in config.yml
    
    Made it so you can now type /despawnentities number; and it will now ignore the config.yml and do whatever amount of blocks you want to do.

--- src/main/java/net/ddns/vcccd/DespawnCommand.java
+++ src/main/java/net/ddns/vcccd/DespawnCommand.java
@@ -8,23 +8,51 @@ import org.bukkit.entity.Player;
 
 public class DespawnCommand implements CommandExecutor {
     private final Main main;
-    
-    public DespawnCommand(Main main){
+
+    public DespawnCommand(Main main) {
         this.main = main;
     }
 
     @Override
-    public boolean  onCommand(CommandSender sender, Command command, String label, String[] args){
-        if(sender instanceof Player){
-            Player player = (Player) sender;
-            for(Entity entity: player.getNearbyEntities(500, 500, 500)){
-                if(!(entity instanceof Player)) {
-                    entity.remove();
-                }
-            }
-            player.sendMessage(main.getPluginPrefix() + "All nearby entities have been despawned.");
-            
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
         }
+
+        Player player = (Player) sender;
+        int radius;
+
+        if (args.length > 0) {
+            try {
+                radius = Integer.parseInt(args[0]);
+                if (radius <= 0) {
+                    player.sendMessage(main.getPluginPrefix() + "Please enter a positive number for radius.");
+                    return true;
+                }
+            } catch (NumberFormatException e) {
+                player.sendMessage(main.getPluginPrefix() + "Invalid number. Usage: /" + label + " [radius]");
+                return true;
+            }
+        } else {
+
+            radius = main.getConfig().getInt("despawn-radius");
+            if (radius <= 0) {
+                player.sendMessage(main.getPluginPrefix() + "Invalid despawn-radius in config.yml. Using default of 500.");
+                radius = 500;
+            }
+        }
+
+        int removedCount = 0;
+        for (Entity entity : player.getNearbyEntities(radius, radius, radius)) {
+            if (!(entity instanceof Player)) {
+                entity.remove();
+                removedCount++;
+            }
+        }
+
+        player.sendMessage(main.getPluginPrefix() +
+                "Despawned " + removedCount + " entities within " + radius + " blocks.");
         return true;
     }
 }
